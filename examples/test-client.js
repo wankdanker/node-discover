9c6f69fe310ac51624d2d4ac447ecd273e5fd079
Dan VerWeire <dverweire@gmail.com>
2015-01-30 11:14:51 -0500

    Add client only option so we just listen

---
+++ examples/test-client.js
@@ -0,0 +1,38 @@
+var Discover = require('../lib/discover.js');
+
+var c = Discover({ client : true});
+
+c.on("promotion", function () {
+	console.log("I was promoted.");
+	
+	c.advertise({
+		RedisMonitor : {
+			protocol : 'tcp',
+			port : 5555
+		}
+	});
+});
+
+c.on("demotion", function () {
+	console.log("I was demoted.");
+	
+	c.advertise(null);
+});
+
+c.on("added", function (obj) {
+	console.log("Node added; here are all the nodes:");
+	c.eachNode(function (node) {
+		console.log(node);
+	});
+});
+
+c.on("removed", function (obj) {
+	console.log("Node removed; here are all the nodes:");
+	c.eachNode(function (node) {
+		console.log(node);
+	});
+});
+
+c.on("master", function (obj) {
+	console.log("New master.");
+});
